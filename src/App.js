2b0dd25787b570609b845709a0040f0dbc410a4c
gpappa01 <gpappa01@ucy.ac.cy>
2022-04-22 15:42:16 +0300

    Fixxed swap

--- src/App.js
+++ src/App.js
@@ -1,85 +1,89 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 let numbers = [];
 let changed = false;
 
-function refresh(){
-    while(true){
-        if (changed){
+function refresh() {
+    while (true) {
+        if (changed) {
             printNumbers();
         }
     }
 }
 
-function printNumbers(){
+function printNumbers() {
     let output;
-    for (let i = 0; i < numbers.length; i++){
+    for (let i = 0; i < numbers.length; i++) {
         output += numbers[i].toString() + " ";
     }
-
     return output;
 }
 
-function generateRandomNumbers(){
-    for(let i = 0; i < 10; i++){
+function generateRandomNumbers() {
+    for (let i = 0; i < 10; i++) {
         numbers.push(Math.random() * 100);
     }
-
-    printNumbers();
+    return numbers;
+    //printNumbers();
 }
 
-function swap(number1, number2){
-    let temp = number1;
-    number1.prop = number2;
-    number2.prop = temp;
+function swap(arr, x1, x2 ) {
+    let temp = arr[x1];
+    arr[x1] = arr[x2];
+    arr[x2] = temp;
 }
 
-function bubbleSort(){
+function bubbleSort() {
     let steps = 0;
     let sorted = false;
 
-    while(!sorted){
+    while (!sorted) {
         sorted = true;
 
         for (let i = 0; i < numbers.length - 1; i++)
-            if (numbers[i] > numbers[i + 1]){
+            if (numbers[i] > numbers[i + 1]) {
                 swap(numbers[i], numbers[i + 1]);
                 sorted = false;
-
                 steps++;
             }
     }
 }
 
 function App() {
-  return (
-      <div>
-          <div>
-              <button onClick={generateRandomNumbers}>Generate Numbers</button>
-          </div>
+    const [array, setArray] = useState([]);
+    console.log("rendering");
+    return (
+        <div>
+            <div>
+                <button onClick={() => {
+                    setArray(generateRandomNumbers)
+                }}>Generate Numbers
+                </button>
+            </div>
 
-          <div>
-              <p></p>
-          </div>
+            <div>
+                <p></p>
+            </div>
 
-          <div>
-              <select>
-                  <option>Ascending</option>
-                  <option>Descending</option>
-              </select>
-          </div>
+            <div>
+                <select>
+                    <option>Ascending</option>
+                    <option>Descending</option>
+                </select>
+            </div>
 
-          <div>
-              <select>
-                  <option onSelect={bubbleSort()}>Bubble Sort</option>
-                  <option>Insertion Sort</option>
-                  <option>Selection Sort</option>
-                  <option>Quick Sort</option>
-              </select>
-          </div>
-      </div>
-  );
+            <div>
+                <select>
+                    <option onSelect={bubbleSort()}>Bubble Sort</option>
+                    <option>Insertion Sort</option>
+                    <option>Selection Sort</option>
+                    <option>Quick Sort</option>
+                </select>
+            </div>
+        </div>
+    );
 }
+
 //test
 export default App;
