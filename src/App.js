b65bf8a1e020a98e40c8daf26c94357a78e91429
gpappa01 <gpappa01@ucy.ac.cy>
2022-04-22 14:25:58 +0300

    Random numbers function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Returns random numbers array
+ * @param count
+ * @param range
+ */
+function randomNumbers(count, range){
+  let x = [];
+  for(let i = 0; i < count; i++){
+    x.push(Math.random()*range + 1);
+  }
+  //1,2,3,4
+  return x.map((num)=>{ return Math.trunc(num) }); //copy
+}
+
 function App() {
   return (
-      <div><h1>cool website</h1></div>
+      <div className="Random numbers">
+        <p>{randomNumbers(10,100).map((num) => {return num + " "})}</p>
+      </div>
   );
 }
 
