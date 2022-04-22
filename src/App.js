75977c7d7cbd8bbfbe00db86754bdc5978b41c09
gpappa01 <gpappa01@ucy.ac.cy>
2022-04-22 14:35:28 +0300

    Random numbers function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+// Our sample dropdown options
+const options = ['BubbleSort', 'InsertionSort', 'SelectionSort', 'QuickSort'];
 
 /**
  * Returns random numbers array
  * @param count
  * @param range
  */
-function randomNumbers(count, range){
-  let x = [];
-  for(let i = 0; i < count; i++){
-    x.push(Math.random()*range + 1);
-  }
-  //1,2,3,4
-  return x.map((num)=>{ return Math.trunc(num) }); //copy
+function randomNumbers(count, range) {
+    let x = [];
+    for (let i = 0; i < count; i++) {
+        x.push(Math.random() * range + 1);
+    }
+    //1,2,3,4
+    return x.map((num) => {
+        return Math.trunc(num)
+    }); //copy
 }
 
 function App() {
-  return (
-      <div className="Random numbers">
-        <p>{randomNumbers(10,100).map((num) => {return num + " "})}</p>
-      </div>
-  );
+    return (
+        <div className="Random numbers">
+            <p>{randomNumbers(10, 100).map((num) => {
+                return num + " "
+            })}</p>
+        </div>
+    );
 }
 
 export default App;
