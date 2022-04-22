f45f043b69f887503ee390fec2f4e1f68534baad
gpappa01 <gpappa01@ucy.ac.cy>
2022-04-22 16:31:33 +0300

    Made a comparison function which will be updated according to choice. ( left > right ) or ( left < right )

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ let numbers = [];
 let changed = false;
 let ascendingOrder = "true"
 
-function refresh() {
-    while (true) {
-        if (changed) {
-            printNumbers();
-        }
-    }
-}
+/** Variable functions. Change according to choices. Don't repeat yourself ;) */
+let comparisonFunction = compareAscending;
+let sortingFunction = bubbleSort;
+
+/** No longer needed */
+// function refresh() {
+//     while (true) {
+//         if (changed) {
+//             printNumbers();
+//         }
+//     }
+// }
+
 
 function printNumbers() {
     let output;
@@ -29,7 +35,9 @@ function generateRandomNumbers() {
     for (let i = 0; i < 10; i++) {
         rands.push(Math.random() * 100);
     }
-    return rands.map((num) => {return Math.trunc(num)});
+    return rands.map((num) => {
+        return Math.trunc(num)
+    });
     //printNumbers();
 }
 
@@ -45,15 +53,41 @@ function swap(arr, i, j) {
     arr[j] = temp;
 }
 
+// function bubbleSort() {
+//     let steps = 0;
+//     let sorted = false;
+//
+//     while (!sorted) {
+//         sorted = true;
+//
+//         for (let i = 0; i < numbers.length - 1; i++)
+//             if (numbers[i] > numbers[i + 1]) {
+//                 swap(numbers, i, i + 1);
+//                 sorted = false;
+//                 steps++;
+//             }
+//     }
+// }
+
+
+function compareAscending(valLeft, valRight) {
+    return valLeft > valRight;
+}
+
+function compareDescending(valLeft, valRight) {
+    return valLeft < valRight;
+}
+
+/**
+ *
+ */
 function bubbleSort() {
     let steps = 0;
     let sorted = false;
-
     while (!sorted) {
         sorted = true;
-
         for (let i = 0; i < numbers.length - 1; i++)
-            if (numbers[i] > numbers[i + 1]) {
+            if (comparisonFunction(numbers[i], numbers[i + 1])) {
                 swap(numbers, i, i + 1);
                 sorted = false;
                 steps++;
@@ -75,25 +109,45 @@ function App() {
 
             <div>
                 <p>
-                    {displayArray.map((num) => {return num + " "})}
+                    {displayArray.map((num) => {
+                        return num + " "
+                    })}
                 </p>
             </div>
 
             <div>
                 <select>
-                    <option onSelect={()=>{ascendingOrder = true}}>Ascending</option>
-                    <option onSelect={()=>{ascendingOrder = false}}>Descending</option>
+                    <option onSelect={() => {
+                        ascendingOrder = true;
+                        comparisonFunction = compareAscending;
+                    }}>Ascending
+                    </option>
+                    <option onSelect={() => {
+                        ascendingOrder = false
+                        comparisonFunction = compareDescending;
+                    }}>Descending
+                    </option>
                 </select>
             </div>
 
             <div>
                 <select>
-                    <option onSelect={bubbleSort()}>Bubble Sort</option>
+                    <option onSelect={()=>{bubbleSort(); setDisplayArray(numbers); console.log("here.")}}>Bubble Sort</option>
                     <option>Insertion Sort</option>
                     <option>Selection Sort</option>
                     <option>Quick Sort</option>
                 </select>
             </div>
+
+            {/*<div>*/}
+            {/*    <select>*/}
+            {/*        //To eixame ylopihsei la8os, o monos pou mporei na exei epiloges einai to select :(*/}
+            {/*        <option onSelect={()=>{bubbleSort(); setDisplayArray(numbers); console.log("here.")}}>Bubble Sort</option>*/}
+            {/*        <option>Insertion Sort</option>*/}
+            {/*        <option>Selection Sort</option>*/}
+            {/*        <option>Quick Sort</option>*/}
+            {/*    </select>*/}
+            {/*</div>*/}
         </div>
     );
 }
